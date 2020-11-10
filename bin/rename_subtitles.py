d048ea2b6408185ea147fd4928e699e78531e4f1
ocecelja <ozprivacy@gmail.com>
2020-11-10 14:15:57 +0100

    added script inside /bin folder

---
+++ bin/rename_subtitles.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import shutil
+
+# define the name of the directory to be created
+path = "./Subs"
+
+if (os.path.isdir(path)):
+    os.rmdir(path)
+
+try:
+    os.mkdir(path)
+except OSError:
+    print ("Creation of the directory %s failed" % path)
+else:
+    print ("Successfully created the directory %s " % path)
+
+# os.rename(r'file_name_old_01.txt', r'file_name_new_01.txt')
+video_file_ext = ['.mkv',
+                 '.mp4',
+                 '.m4v',
+                 '.m4p',
+                 '.wmv',
+                 '.flv',
+                 '.f4v',
+                 '.f4p',
+                 '.f4a',
+                 '.f4b']
+
+subtitles_file_ext = ['.srt',
+                      '.lrc',
+                      '.ttml',
+                      '.ccml',
+                      '.ttxt',
+                      '.mp4tt',
+                      '.sami',
+                      '.ssa',
+                      '.smil',
+                      '.usf',
+                      '.sub']
+
+file_list = []
+subtitles_file_list = []
+video_file_list = []
+file_list = os.listdir('.')
+# for root, files in os.walk("."):
+#     for filename in files:
+#         print(filename)
+#         file_list.append(filename)
+
+for file in file_list:
+    for extension in subtitles_file_ext:
+        if file.endswith(extension):
+            ext = file.split(extension)
+            subtitles_file_list.append(ext[0])
+            if (len(ext) > 2):
+                os.rename(file, ext[0] + extension)
+
+    for extension in video_file_ext:
+        if file.endswith(extension):
+            ext = file.split(extension)
+            video_file_list.append(ext[0])
+
+print("\n")
+
+print("subtitles_file_list:")
+for file in subtitles_file_list:
+    print(file)
+print("\n")
+
+print("video_file_list:")
+for file in video_file_list:
+    print(file)
+print("\n")
+
+for file_x in subtitles_file_list:
+    x = re.findall('\d{2}', file_x)
+    x = x.pop()
+    if x:
+        for file_y in video_file_list:
+            y = re.findall(x, file_y)
+            if y:
+                os.rename(file_x + ".srt", file_y + ".srt")
+                shutil.copy(file_y + ".srt", path.format(os.path.basename(file_y)))
+
+print("Final file list:")
+file_list = os.listdir('.')
+for file in file_list:
+    print(file)
+
+input('Press Enter to Continue...')
